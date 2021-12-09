1946eb51db9886b247dd3f024eabff011ea1e8a7
KinOnik <84587417+KinOnik@users.noreply.github.com>
2021-12-09 15:53:35 +0300

    Add files via upload

---
+++ 4form/4form/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace _4form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(476, 160);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Результат";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(26, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Размеры массива (NxN):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBox1.Location = new System.Drawing.Point(26, 75);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 32);
+            this.textBox1.TabIndex = 2;
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.result.Location = new System.Drawing.Point(26, 230);
+            this.result.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(124, 26);
+            this.result.TabIndex = 3;
+            this.result.Text = "Результат:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.Location = new System.Drawing.Point(341, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Больше какого числа:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBox2.Location = new System.Drawing.Point(341, 75);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 32);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 555);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ 4form/4form/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                result.Text = ("Результат:");
+                int[][] myArray = Input();
+                int sizeArray = myArray.GetLength(0);
+                for (int i = 0; i < sizeArray; i++)
+                {
+                    result.Text+=("\n\t");
+                    for (int j = 0; j < sizeArray; j++)
+                    {
+
+                        result.Text +=($"{myArray[i][j]} ");
+                    }
+                }
+
+                int[] countMore = countingMore(myArray);
+
+                result.Text += ("\n\nЧисел в строках превышающих:");
+                for (int i = 0; i < sizeArray; i++)
+                {
+
+                    result.Text += ($"\n\t{countMore[i]}");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(
+                         "Вы ввели не число!",
+                         "Предупреждение",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Warning,
+                         MessageBoxDefaultButton.Button3);
+            }
+
+        }
+
+        int[][] Input()
+        {
+            Random rnd = new Random();
+            int n = int.Parse(textBox1.Text);
+            int[][] a = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = new int[n];
+                for (int j = 0; j < n; j++)
+                {
+                    a[i][j] = rnd.Next(0,100);
+                }
+            }
+            return a;
+        }
+
+        int[] countingMore(int[][] a)
+        {
+            try
+            {
+                int n = a.Length;
+                int whoMore = int.Parse(textBox2.Text);
+                int[] count = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    count[i] = 0;
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (a[i][j] > whoMore)
+                            count[i]++;
+                    }
+                }
+                return count;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
